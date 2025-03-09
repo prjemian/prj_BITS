aafddccdf5e6c30d70a692a086ab41a83dee86c6
Pete R Jemian <prjemian@users.noreply.github.com>
2025-03-09 10:41:26 -0500

    MNT #3 refactor as try..except

--- src/instrument/core/catalog_init.py
+++ src/instrument/core/catalog_init.py
@@ -18,10 +18,10 @@ logger.bsdev(__file__)
 TEMPORARY_CATALOG_NAME = "temp"
 
 catalog_name = iconfig.get("DATABROKER_CATALOG", TEMPORARY_CATALOG_NAME)
-if catalog_name == TEMPORARY_CATALOG_NAME:
-    _cat = databroker.temp().v2
-else:
+try:
     _cat = databroker.catalog[catalog_name].v2
+except KeyError:
+    _cat = databroker.temp().v2
 
 cat = _cat
 """Databroker catalog object, receives new data from ``RE``."""
